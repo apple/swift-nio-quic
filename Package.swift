06f5e278519d2080ccd2c67527ebc7f616e7e947
Raphael Hiesgen <r_hiesgen@apple.com>
2026-07-27 17:19:21 +0100

    New swift-network-evolution tag

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-quic-helpers.git", .upToNextMinor(from: "0.1.0")),
         .package(
             url: "https://github.com/apple/swift-network-evolution",
-            .upToNextMinor(from: "0.1.0"),
+            .upToNextMinor(from: "0.1.1"),
             traits: swiftNetworkTraits
         ),
         .package(url: "https://github.com/apple/swift-tls", .upToNextMinor(from: "0.1.0")),
